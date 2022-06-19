e8f8f9eb74a60b17109d10abcd20556c7172adf4
Ravil <ravilgab@yahoo.com>
2022-06-19 18:38:21 +0300

    Project is done – added enum for light switch and button appearence is done with code.

--- Homework2_2.28/ViewControllers/TrafficLightViewController.swift
+++ Homework2_2.28/ViewControllers/TrafficLightViewController.swift
@@ -14,18 +14,22 @@ class TrafficLightViewController: UIViewController {
     @IBOutlet weak var greenView: UIView!
     
     @IBOutlet weak var startButton: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
+    
+    enum Colors {
+        case red
+        case yellow
+        case green
+    }
     
     private let brightLight: CGFloat = 1
     private let dimLight: CGFloat = 0.2
-    
-    private var currentColor = 0
+
+    private var currentColor = Colors.red
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        startButton.tintColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
-        resetButton.tintColor = UIColor.lightGray
+        startButton.configuration = setupButton(title: "Start")
         
         redView.layer.backgroundColor = #colorLiteral(red: 1, green: 0.0187217119, blue: 0, alpha: 1)
         yellowView.layer.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
@@ -34,8 +38,6 @@ class TrafficLightViewController: UIViewController {
         redView.alpha = dimLight
         yellowView.alpha = dimLight
         greenView.alpha = dimLight
-
-        startButton.layer.cornerRadius = 10
     }
     
     override func viewDidLayoutSubviews() {
@@ -48,34 +50,30 @@ class TrafficLightViewController: UIViewController {
     
     @IBAction func startButtonPressed() {
         startButton.setTitle("Next", for: .normal)
-        resetButton.tintColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
         
         switch currentColor {
-        case 0:
+        case .red:
             redView.alpha = brightLight
             greenView.alpha = dimLight
-            currentColor = 1
-        case 1:
+            currentColor = Colors.yellow
+        case .yellow:
             yellowView.alpha = brightLight
             redView.alpha = dimLight
-            currentColor = 2
-        case 2:
+            currentColor = Colors.green
+        case .green:
             greenView.alpha = brightLight
             yellowView.alpha = dimLight
-            currentColor = 0
-        default:
-            break
+            currentColor = Colors.red
         }
     }
     
-    @IBAction func resetButtonPressed() {
-        startButton.setTitle("Start", for: .normal)
-        resetButton.tintColor = UIColor.lightGray
+    private func setupButton(title: String) -> UIButton.Configuration {
+        var buttonConfiguration = UIButton.Configuration.filled()
         
-        currentColor = 0
-        
-        redView.alpha = dimLight
-        yellowView.alpha = dimLight
-        greenView.alpha = dimLight
-    }    
+        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
+        buttonConfiguration.title = title
+        buttonConfiguration.cornerStyle = .large
+
+        return buttonConfiguration
+    }
 }
